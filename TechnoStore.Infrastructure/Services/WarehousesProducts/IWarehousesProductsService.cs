acf7b7e48bdb39a7da5b5ca20e7dd397c0766330
Ali Abu Alatta <ali-rafeq-1997@hotmail.com>
2022-03-18 22:00:08 +0200

    edit interface warehouse

--- TechnoStore.Infrastructure/Services/WarehousesProducts/IWarehousesProductsService.cs
+++ TechnoStore.Infrastructure/Services/WarehousesProducts/IWarehousesProductsService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechnoStore.Core.Dto.WarehousesProducts;
 using TechnoStore.Core.ViewModel.WarehousesProducts;
 
 namespace TechnoStore.Infrastructure.Services.WarehousesProducts
@@ -11,5 +12,9 @@ namespace TechnoStore.Infrastructure.Services.WarehousesProducts
     {
         Task<WarehouseProductDetailsVm> GetProductDetails(int id);
         Task<warehouseProductForWarehouseDetailsVm> GetWarehouseDetails(int id);
+        Task<List<WarehouseProductVm>> GetAll(string search, int page);
+        Task<List<WarehouseProductVm>> GetAll();
+        Task<bool> Save(string userId, CreateWarehouseProductDto dto);
+
     }
 }
